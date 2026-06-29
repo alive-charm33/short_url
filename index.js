0161d56de39be93bde1c292647abe27ae58fa90b
Sejal <bargalesejal@gmail.com>
2026-06-29 17:07:33 +0530

    feat: add Mongoose URL model for short URL storage

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express=require ('express');
+const {connectToMongoDB}=require("./connect");
+const urlRoute=require('./routes/url');
 const app=express();
 const PORT=8001;
+connectToMongoDB('mongodb://localhost:27017/short_url').then(()=>
+console.log('mongoDB connected'))
+app.use("/url",urlRoute);
 app.listen(PORT,()=>console.log('Server started ast PORT'));
--- routes/url.js
+++ routes/url.js
@@ -1,15 +1,6 @@
 const express = require("express");
+const {handleGenerateNewShortURL}=require("../controllers/url");
 const router = express.Router();
-
-const {
-    handleGenerateNewShortURL,
-    handleGetAnalytics,
-} = require("../controllers/url");
-
 // POST /url
 router.post("/", handleGenerateNewShortURL);
-
-// GET /url/analytics/:shortId
-router.get("/analytics/:shortId", handleGetAnalytics);
-
 module.exports = router;
